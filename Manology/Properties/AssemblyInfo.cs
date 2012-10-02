e54711b53c4ef61f381d280663835fbb89d70d1c
Alexander Skakovskiy <optimusway@gmail.com>
2012-10-02 18:03:11 +0400

    updated index view on manology.info

---
+++ Manology/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Manology")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Manology")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cf8e34e9-0914-4b1b-b44a-f61bfa6d6f5f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Manology/Scripts/app.js
@@ -0,0 +1,40 @@
+﻿$(function () {
+	$('[data-info]')
+		.on('click', function (e) {
+			infoblockId = '#' + $(this).attr('data-info');
+
+			$info = $(infoblockId);
+
+			if ($info.html() == '') {
+				$(infoblockId)
+					.load('/home/about', function (data) {
+						console.log(data);
+					})
+			}
+
+			if ($(this).hasClass('infoblockOpen')) {
+				$info
+					.animate({
+						top: '-450px'
+					}, 800);
+				$(this).removeClass('infoblockOpen');
+			}
+			else {
+				$info
+					.animate({
+						top: '0px'
+					}, 800);
+				$(this).addClass('infoblockOpen');
+			}
+		});
+
+	$(document).keyup(function (e) {
+		if (e.keyCode == 27) {
+			$('#infoblock')
+				.animate({
+					top: '-450px'
+				}, 800);
+			$('.tip').removeClass('infoblockOpen');
+		}
+	});
+})
